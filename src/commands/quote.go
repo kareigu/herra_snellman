8e32573e08cebe5d13ec307a18ae681b8d815187
karei <31644842+kareigu@users.noreply.github.com>
2022-05-08 15:09:30 +0300

    Update domain

--- src/commands/quote.go
+++ src/commands/quote.go
@@ -33,7 +33,7 @@ func quoteCmd(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		search_param = i.Data.Options[0].StringValue()
 	}
 
-	url := fmt.Sprintf("https://misi.mxrr.dev/api/v1/quotes?search=%s", search_param)
+	url := fmt.Sprintf("https://misi.karei.dev/api/v1/quotes?search=%s", search_param)
 	text := ""
 	err := getData(url, &quote)
 	if err != nil {
